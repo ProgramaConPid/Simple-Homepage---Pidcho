7dca868319787801de8a06055871271a78b57353
Felipe Marin <138628056+ProgramaConPid@users.noreply.github.com>
2024-03-29 01:15:53 -0500

    Check!

---
+++ index.js
@@ -0,0 +1,10 @@
+const iconoMenu = document.getElementById('icon__bars');
+const navLinks = document.querySelector('.nav__links');
+
+iconoMenu.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+})
+
+
+
+
